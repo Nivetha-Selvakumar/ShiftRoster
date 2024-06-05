f351dede6e18818ad0af821f9a2e27c4f47063b3
Nivetha-Selvakumar <nivethaselvakumar23@gmail.com>
2024-06-05 12:52:29 +0530

    first commit

---
+++ src/main/java/com/example/shiftroster/ShiftrosterApplication.java
@@ -0,0 +1,13 @@
+package com.example.shiftroster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftrosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftrosterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shiftroster/ShiftrosterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shiftroster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftrosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
