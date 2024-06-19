a9df5b1eae5113b9997578de67abbdc63b7217aa
Nivetha-Selvakumar <nivethaselvakumar23@gmail.com>
2024-06-19 11:01:40 +0530

    Email employee validation added

--- src/main/java/com/example/shiftroster/persistence/controller/EmailCronController.java
+++ src/main/java/com/example/shiftroster/persistence/controller/EmailCronController.java
@@ -1,14 +1,14 @@
 package com.example.shiftroster.persistence.controller;
 
+import com.example.shiftroster.persistence.Exception.CommonException;
 import com.example.shiftroster.persistence.cron.ReminderScheduler;
 import com.example.shiftroster.persistence.util.AppConstant;
+import com.example.shiftroster.persistence.validation.BasicValidation;
+import com.example.shiftroster.persistence.validation.BusinessValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
@@ -18,8 +18,16 @@ public class EmailCronController {
     @Autowired
     ReminderScheduler reminderScheduler;
 
+    @Autowired
+    BasicValidation basicValidation;
+
+    @Autowired
+    BusinessValidation businessValidation;
+
     @GetMapping(path = "/dailycron/shift")
-    public ResponseEntity<String> setReminder()  {
+    public ResponseEntity<String> setReminder(@RequestHeader String empId) throws CommonException {
+        basicValidation.empIdValidation(empId);
+        businessValidation.employeeValidation(empId);
         reminderScheduler.sendReminderTask();
         return ResponseEntity.status(HttpStatus.CREATED).body(AppConstant.REMINDER_SENT_SUCCESSFULLY);
     }
--- src/test/java/com/example/shiftroster/persistence/controller/EmailCronControllerTest.java
+++ src/test/java/com/example/shiftroster/persistence/controller/EmailCronControllerTest.java
@@ -1,5 +1,7 @@
 package com.example.shiftroster.persistence.controller;
 
+import com.example.shiftroster.persistence.Exception.CommonException;
+import com.example.shiftroster.persistence.Exception.MisMatchException;
 import com.example.shiftroster.persistence.cron.ReminderScheduler;
 import com.example.shiftroster.persistence.util.AppConstant;
 import org.junit.jupiter.api.Assertions;
@@ -19,10 +21,10 @@ class EmailCronControllerTest {
     ReminderScheduler reminderScheduler;
 
     @Test
-    void setReminderTest(){
-        reminderScheduler.sendReminderTask();
+    void setReminderTest() throws CommonException {
+        reminderScheduler.sendReminderTask("1");
         String expected = AppConstant.REMINDER_SENT_SUCCESSFULLY;
-        ResponseEntity<String> actual = emailCronController.setReminder(); ;
+        ResponseEntity<String> actual = emailCronController.setReminder("1"); ;
         Assertions.assertEquals(expected, actual.getBody());
     }
 }
